13db9e2955b434091a2aa2923a1fc86120edd962
Isaac Sastre <isaac.sastre@techcosecurity.com>
2020-01-08 15:02:41 +0100

    Change IUnitOfWork to Core

--- source/PTAnalitic/PTAnaliticCore/Extensions/ServiceCollectionExtensions.cs
+++ source/PTAnalitic/PTAnaliticCore/Extensions/ServiceCollectionExtensions.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PTAnalitic.Infrastructure.Extensions;
 
 namespace PTAnalitic.Core.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        public static void AddServices(this IServiceCollection services)
         {
-            services.AddRepositories(configuration);
+            
         }
     }
 }
--- source/PTAnalitic/PTAnaliticRepository/Extensions/ServiceCollectionExtensions.cs
+++ source/PTAnalitic/PTAnaliticRepository/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PTAnalitic.Infrastructure.UnitOfWork;
+using PTAnalitic.Core.UnitOfWork;
 
 namespace PTAnalitic.Infrastructure.Extensions
 {
